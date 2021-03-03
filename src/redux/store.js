3892fb1f3cfb0fd56451ddf0f04d733d68ae7104
Harshilmehta23 <mharshil60@gmail.com>
2021-03-03 15:23:28 +0530

    Created and moved code of collections and single collection page into cotainer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 export const store = createStore(
   rootReducer,
