1f3470869a0fe8f3b8e2bd6076ae50ca67433ba1
Harshilmehta23 <mharshil60@gmail.com>
2021-03-05 15:17:04 +0530

    converted all code over tobe mobile friendly

--- client/src/components/sign-in/sign-in.component.jsx
+++ client/src/components/sign-in/sign-in.component.jsx
@@ -45,7 +45,7 @@ const SignIn = ({ emailSignInStart, googleSignInStart }) => {
           type='email'
           handleChange={handleChange}
           value={email}
-          label='email'
+          label='Email'
           required
         />
         <FormInput
@@ -53,7 +53,7 @@ const SignIn = ({ emailSignInStart, googleSignInStart }) => {
           type='password'
           value={password}
           handleChange={handleChange}
-          label='password'
+          label='Password'
           required
         />
         <ButtonsBarContainer>
